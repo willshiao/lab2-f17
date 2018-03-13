db23f10ee693c0067721de95882ad2e806ddba8c
William Shiao <willshiao@gmail.com>
2018-03-13 04:00:26 -0700

    Fix bug with shared memory

--- shm_cnt.c
+++ shm_cnt.c
@@ -4,44 +4,46 @@
 #include "uspinlock.h"
 
 struct shm_cnt {
-   struct uspinlock lock;
-   int cnt;
+  struct uspinlock lock;
+  int cnt;
 };
 
-int main(int argc, char *argv[])
-{
-int pid;
-int i=0;
-struct shm_cnt *counter;
-  pid=fork();
+int main(int argc, char *argv[]) {
+  int pid;
+  int i = 0;
+  struct shm_cnt *counter;
+
+  pid = fork();
   sleep(1);
 
-//shm_open: first process will create the page, the second will just attach to the same page
-//we get the virtual address of the page returned into counter
-//which we can now use but will be shared between the two processes
+  //shm_open: first process will create the page, the second will just attach to the same page
+  //we get the virtual address of the page returned into counter
+  //which we can now use but will be shared between the two processes
   
-shm_open(1,(char **)&counter);
+  shm_open(1,(char **)&counter);
  
-//  printf(1,"%s returned successfully from shm_open with counter %x\n", pid? "Child": "Parent", counter); 
-  for(i = 0; i < 10000; i++)
-    {
-     uacquire(&(counter->lock));
-     counter->cnt++;
-     urelease(&(counter->lock));
+  // printf(1,"%s returned successfully from shm_open with counter %x\n", pid? "Child": "Parent", counter); 
+  for(i = 0; i < 10000; i++) {
+    // printf(1, "Trying to acquire lock!\n");
+    uacquire(&(counter->lock));
+    // printf(1, "Got lock!\n");
+    counter->cnt++;
+    urelease(&(counter->lock));
 
-//print something because we are curious and to give a chance to switch process
-     if(i%1000 == 0)
-       printf(1,"Counter in %s is %d at address %x\n",pid? "Parent" : "Child", counter->cnt, counter);
-}
-  
-  if(pid)
-     {
-       printf(1,"Counter in parent is %d\n",counter->cnt);
+    //print something because we are curious and to give a chance to switch process
+    if(i%1000 == 0) {
+      printf(1,"Counter in %s is %d at address %x\n",pid? "Parent" : "Child", counter->cnt, counter);
+    }
+  }
+
+  if(pid) {
+    printf(1,"Counter in parent is %d\n",counter->cnt);
     wait();
-    } else
+  } else {
     printf(1,"Counter in child is %d\n\n",counter->cnt);
+  }
 
-//shm_close: first process will just detach, next one will free up the shm_table entry (but for now not the page)
+  //shm_close: first process will just detach, next one will free up the shm_table entry (but for now not the page)
    shm_close(1);
    exit();
    return 0;
